765ba6d5982f17454658448e6be5949d772a874d
Rori <rbmasilo@gmail.com>
2024-08-05 00:46:37 +0200

    Added vue-router dependency, updated package-lock.json and package.json, removed HelloWorld.vue component, and modified App.vue to use router-view

---
+++ SwiftCart/src/components/ProductCard.vue
@@ -0,0 +1,22 @@
+<template>
+  <div @click="navigateToDetails" class="card">
+    <img :src="product.image" alt="Product Image" />
+    <h3>{{ product.title }}</h3>
+    <p>{{ product.category }}</p>
+    <p>{{ product.price | currency }}</p>
+  </div>
+</template>
+
+<script setup>
+import { useRouter } from "vue-router";
+
+const props = defineProps({
+  product: Object,
+});
+
+const router = useRouter();
+
+const navigateToDetails = () => {
+  router.push(`/product/${props.product.id}`);
+};
+</script>
