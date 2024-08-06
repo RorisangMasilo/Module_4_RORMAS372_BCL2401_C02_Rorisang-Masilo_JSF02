565f12115da62a166798f55d52fc017982eee8ea
Rori <rbmasilo@gmail.com>
2024-08-06 16:43:13 +0200

    Updated ProductCard.vue component: changed image styling, reformatted product title, rating, price, and category displays, and modified button styling.

--- SwiftCart/src/components/ProductCard.vue
+++ SwiftCart/src/components/ProductCard.vue
@@ -20,24 +20,30 @@ const navigateToDetails = () => {
     <img
       :src="product.image"
       alt="Product Image"
-      class="p-8 rounded-t-lg bg-contain"
+      class="object-contain h-48 mt-3"
     />
-    <h1
-      class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white"
-    >
-      {{ product.title }}
-    </h1>
+    <div class="mb-2 flex-2">
+      <h2 class="text-lg line-clamp-2 font-extrabold leading-snug">
+        {{ product.title }}
+      </h2>
+    </div>
     <div class="flex items-center mt-2.5 mb-5">
       <!-- Render the rating here -->
       <span
-        class="bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ms-3"
-        >{{ product.rating.rate }}</span
+        class="text-slate-600 focus-visible:outline-none focus-visible:ring focus-visible:ring-indigo-300"
+        >{{ product.rating }}</span
       >
     </div>
-    <h4 class="text-3xl font-bold text-gray-900 dark:text-white">
+    <h4
+      class="text-base line-clamp-2 font-extrabold text-slate-500 leading-snug"
+    >
       ${{ product.price }}
     </h4>
-    <p class="text-gray-600 mb-2">{{ product.category }}</p>
+    <p
+      class="inline-flex items-center rounded-md bg-blue-50 px-2 py-1 text-xs font-medium text-blue-700 ring-1 ring-inset ring-blue-700/10"
+    >
+      {{ product.category }}
+    </p>
     <button
       @click="$emit('add-to-cart', product)"
       class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
