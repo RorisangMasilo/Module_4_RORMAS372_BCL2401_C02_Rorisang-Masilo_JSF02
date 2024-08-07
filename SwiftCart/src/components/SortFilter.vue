f19b41fcb5f13dbfe52e3aab42f09888826fd54b
Rori <rbmasilo@gmail.com>
2024-08-07 15:48:45 +0200

    Refactored SortFilter.vue component layout and styling

--- SwiftCart/src/components/SortFilter.vue
+++ SwiftCart/src/components/SortFilter.vue
@@ -1,28 +1,36 @@
 <template>
   <!-- Filtering -->
-  <div class="flex sm:w-[95%] max-w-[21rem] md:w-full">
-    <select
-      v-model="selectedCategory"
-      @change="filterProducts"
-      class="flex-shrink-0 z-10 inline-flex items-center py-2.5 px-4 text-sm font-medium text-center text-gray-900 bg-gray-100 border border-gray-300 rounded-s-lg hover:bg-gray-200"
-    >
-      <option value="">All Categories</option>
-      <option v-for="category in categories" :key="category" :value="category">
-        {{ category }}
-      </option>
-    </select>
+  <div>
+    <div class="flex lg:w-[31.25rem] sm:w-[95%] md:w-full relative">
+      <select
+        v-model="selectedCategory"
+        @change="filterProducts"
+        class="flex-shrink-0 z-10 inline-flex items-center py-2.5 px-4 text-sm font-medium text-center text-gray-900 bg-gray-100 border border-gray-300 rounded-s-lg hover:bg-gray-200"
+      >
+        <option value="">All Categories</option>
+        <option
+          v-for="category in categories"
+          :key="category"
+          :value="category"
+        >
+          {{ category }}
+        </option>
+      </select>
+    </div>
 
     <!-- Sorting -->
-    <label :for="'sort'" class="w-20 my-auto font-semibold"> Sort by: </label>
-    <select
-      v-model="sortOrder"
-      @change="sortProducts"
-      class="p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-e-lg border-s-gray-50 border-s-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
-    >
-      <option value="default">Default</option>
-      <option value="lowest">Price: Low to High</option>
-      <option value="highest">Price: High to Low</option>
-    </select>
+    <div class="flex sm:w-[95%] max-w-[21rem] md:w-full">
+      <label :for="sort" class="w-20 my-auto font-semibold"> Sort by: </label>
+      <select
+        v-model="sortOrder"
+        @change="sortProducts"
+        class="p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-e-lg border-s-gray-50 border-s-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
+      >
+        <option value="default">Default</option>
+        <option value="lowest">Price: Low to High</option>
+        <option value="highest">Price: High to Low</option>
+      </select>
+    </div>
   </div>
 </template>
 <script>
